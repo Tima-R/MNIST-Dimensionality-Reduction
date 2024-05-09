34a043b5f59ac649e8c506ecac9fc273a0e4f803
Tima-R <116596345+Tima-R@users.noreply.github.com>
2024-05-09 23:52:31 +0100

    Create scripts.py

---
+++ scripts.py
@@ -0,0 +1,12 @@
+
+#load_data               
+display_samples          
+preprocess_data          
+apply_pca             
+visualize_pca_components
+check_variance           
+cluster_kmeans           
+classify_logistic       
+visualize_2d_pca        
+visualize_tsne         
+visualize_umap
